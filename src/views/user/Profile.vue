dd7142cbee6f3a976db5d15d930bcc44cf48ef30
fireworks99 <46671672+fireworks99@users.noreply.github.com>
2026-03-04 17:31:12 +0800

    个人信息

--- src/views/user/Profile.vue
+++ src/views/user/Profile.vue
@@ -1,3 +1,46 @@
 <template>
-  <div>个人信息</div>
+  <el-card style="max-width: 500px; margin: 40px auto;">
+    <template #header>
+      <span>个人信息</span>
+    </template>
+
+    <el-descriptions :column="1" border label-width="80">
+      <el-descriptions-item label="用户标识">
+        {{ userId }}
+      </el-descriptions-item>
+      <el-descriptions-item label="用户名">
+        {{ username }}
+      </el-descriptions-item>
+      <el-descriptions-item label="Token">
+        <span class="token">{{ token }}</span>
+      </el-descriptions-item>
+      <el-descriptions-item label="过期时间">
+        {{ dayjs(tokenExp).format('YYYY-MM-DD HH:mm:ss') }}
+      </el-descriptions-item>
+    </el-descriptions>
+  </el-card>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import dayjs from 'dayjs';
+
+const userId = ref(localStorage.getItem('userId'));
+const username = ref(localStorage.getItem('username'));
+const token = ref(localStorage.getItem('token'));
+const tokenExp = ref(localStorage.getItem('token_exp'));
+</script>
+
+<style scoped>
+.token {
+  word-break: break-all;
+  font-size: 12px;
+  color: #666;
+}
+</style>
+
+<style>
+.el-descriptions__label.el-descriptions__cell.is-bordered-label {
+  text-align: center;
+}
+</style>
--- src/views/user/Security.vue
+++ src/views/user/Security.vue
@@ -1,3 +1,40 @@
 <template>
-  <div>安全设置</div>
+  <el-card style="max-width: 500px; margin: 40px auto;">
+    <template #header>
+      <span>安全设置</span>
+    </template>
+
+    <el-descriptions :column="1" border label-class-name="center-label">
+      <el-descriptions-item label="修改密码">
+        <el-button type="primary" size="small" @click="changePassword">修改</el-button>
+      </el-descriptions-item>
+
+      <el-descriptions-item label="两步验证">
+        <el-switch v-model="twoFactor" active-color="#13ce66"></el-switch>
+      </el-descriptions-item>
+
+      <el-descriptions-item label="最近登录设备">
+        <ul style="padding-left: 20px; margin: 0;">
+          <li v-for="(device, index) in devices" :key="index">{{ device }}</li>
+        </ul>
+      </el-descriptions-item>
+    </el-descriptions>
+  </el-card>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const twoFactor = ref(false)
+const devices = ref(['Chrome - Windows 10', 'Firefox - macOS', 'iPhone App'])
+
+function changePassword() {
+  
+}
+</script>
+
+<style scoped>
+.center-label {
+  text-align: center !important;
+}
+</style>
