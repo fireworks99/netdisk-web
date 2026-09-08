fbf278b58f4e3eb35451e74a221356191cf003dc
fireworks99 <fireworks99@users.noreply.github.com>
2026-09-08 09:27:34 +0000

    deploy: 6f2470a305b18fa53bac34b02e221582fdcdac00

---
+++ assets/favorite-g2iLCLV8.js
@@ -0,0 +1,1 @@
+import{B as e}from"./index-MC1vnNtS.js";function o(r){return e({url:"/favorite/page",params:r})}function a(r){return e({url:`/favorite/${r}`,method:"post"})}function i(r){return e({url:`/favorite/${r}`,method:"delete"})}export{o as g,i as r,a as s};
